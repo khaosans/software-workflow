b8f3820ca537a7aeb024095e099a28b0554faebe
SourC <souriya.khaosanga@gmail.com>
2024-08-11 00:42:04 +0000

    load credentials

--- team.py
+++ team.py
@@ -1,3 +1,20 @@
+import getpass
+import os
+
+
+def _set_if_undefined(var: str):
+    if not os.environ.get(var):
+        os.environ[var] = getpass.getpass(f"Please provide your {var}")
+
+
+_set_if_undefined("OPENAI_API_KEY")
+_set_if_undefined("LANGCHAIN_API_KEY")
+_set_if_undefined("TAVILY_API_KEY")
+
+# Optional, add tracing in LangSmith
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_PROJECT"] = "Multi-agent Collaboration"
+
 from langchain.prompts import ChatPromptTemplate
 from langchain.chains import LLMChain
 from langchain.chat_models import ChatOpenAI
